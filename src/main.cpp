3c82216b383d57b0a003b986ad8e078f307d0ddb
1832jsh <78774355+1832jsh@users.noreply.github.com>
2022-11-07 09:47:31 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-// Author: Alan Manuel Loreto Cornídez
-// Net ID: aloretocornidez
-// Date: October 3rd, 2022
-// Assignment: Lab 3
+// Author:
+// Net ID:
+// Date:
+// Assignment:     Lab 3
 //
 // Description: This file contains a programmatic overall description of the
 // program. It should never contain assignments to special function registers
@@ -10,147 +10,102 @@
 // Requirements:
 //----------------------------------------------------------------------//
 
+
+
+
+#include <Arduino.h>
+
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include "led.h"
-#include "switch.h"
+#include "7seg.h"
 #include "timer.h"
-#include "lcd.h"
+#include "switch.h"
+#include <avr/interrupt.h>
 
+#define wt_press 0
+#define db_press 1
+#define wt_release 2
+#define db_release 3
+int btncase = 0;
+int motorState = 0;
+#define on 1
+#define off 0
 // defines
-#define SHORT_DELAY 200
-#define LONG_DELAY 100
 
+int num =0;
+int cds = 0;
 /*
  * Define a set of states that can be used in the state machine using an enum.
  */
-typedef enum stateType_enum
-{
-        wait_press,
-        debounce_press,
-        wait_release,
-        debounce_release
-} stateType;
-
-volatile stateType state = wait_press;
-
-// A boolean variable to keep track of the LED speed.
-bool fastLedSpeed = true;
-
-int main()
-{
-        // Initialize a char variable to keep track of the count on the LEDs.
-        unsigned char count = '0';
-
-        // Seting up exterinal interrupts.
-        // Initialize all of the components.
-        // Turns on the LED pins for correct output.
-        // Initializes the switch.
-        // Initializes the Timer0.
-        initTimer1();
-        initTimer0();
-        initLCD();
-        initSwitchPB3();
-        initLED();
-        sei();
+// typedef enum .......
 
 
-        // TODO: SET UP THESE FUNCTIONS
-        moveCursor(0, 0); // moves the cursor to 0,0 position
-        writeString("Current Mode:");
-        moveCursor(1, 0); // moves the cursor to 1,0 position
-        writeString("Slow");
+// Initialize states.  Remember to use volatile 
 
 
-        // // Seting up exterinal interrupts.
-        // sei();
-        // // Initialize all of the components.
-        // // Turns on the LED pins for correct output.
-        // initLED();
-        // // Initializes the switch.
-        // initSwitchPB3();
-        // // Initializes the Timer0.
-        // initTimer0();
-
-
-        // // TODO: SET UP THESE FUNCTIONS
-        // initTimer1();
-        // initLCD();
-        // moveCursor(0, 0); // moves the cursor to 0,0 position
-        // writeString("Current mode: ");
-        // moveCursor(1, 0); // moves the cursor to 1,0 position
-
-        // while loop
-        while (1)
-        {
-
-                // Delay the system by a certain amount of time
-                if (fastLedSpeed)
-                {
-                        delayMs(SHORT_DELAY);
-                        // Change the LEDs by the count.
-                        turnOnLEDWithChar(count);
-                }
-                else
-                {
-                        delayMs(LONG_DELAY);
-                        // Change the LEDs by the count.
-                        turnOnLEDWithChar(count);
-                }
-
-                switch (state)
-                {
-                case wait_press:
-                        break;
-
-                case debounce_press:
-                        delayMs(1);
-                        state = wait_release;
-                        break;
-
-                case wait_release:
-                        break;
-
-                case debounce_release:
-                        delayMs(1);
-                        state = wait_press;
-                        break;
-                }
-
-                // increment the counter
-                count++;
-                // modulo the counter
-                count %= 16;
-        }
-        return 0;
+void countDown(){
+  int cdt = 9;
+  while(cdt >= 0){
+  turnOn7SegWithNum(cdt);
+  delayS(1);
+  cdt--;
+  }
+  turnOn7SegWithNum(-1);
 }
 
-/* Implement an Pin Change Interrupt which handles the switch being
- * pressed and released. When the switch is pressed and released, the LEDs
- * change at twice the original rate. If the LEDs are already changing at twice
- * the original rate, it goes back to the original rate.
- */
-ISR(PCINT0_vect)
-{
-        // This function is hit when the button is presssed
-        if (state == wait_press)
-        {
-                state = debounce_press;
-        }
-        else if (state == wait_release)
-        {
-                // Do nothing while waiting
-                if (fastLedSpeed == true)
-                { // if the led_speed is fast then change it to slow
-                        fastLedSpeed = false;
-                        moveCursor(1, 0);
-                        writeString("Fast");
-                }
-                else
-                {
-                        moveCursor(1, 0);
-                        writeString("Slow");
-                        fastLedSpeed = true; // else led_speed was slow so change it to fast
-                }
-                state = debounce_release;
-        }
+int main(){
+  Serial.begin(9600);
+  init7Seg();
+  initTimer0();
+  initTimer1();
+  initSwitchPD0();
+  sei();
+
+
+
+  while(1){
+    if(num > 9){
+      num =0;
+    }
+    if(cds == on){
+      countDown();
+      cds = off;
+    }
+  
+  switch(btncase) { //debounce button
+   case wt_press:
+        break;
+   case db_press:
+        delayMs(1);
+        btncase = wt_release;
+        break;
+    case wt_release:
+        break;
+    case db_release:
+        delayMs(1);
+        btncase = wt_press;
+        break;          
+    }
+
+  }
+  }
+
+  ISR(INT0_vect){ //ISR
+  if( btncase == wt_press) { //move through state machine based on button
+    btncase = db_press;
+  }
+  else if (btncase == wt_release) {
+    Serial.println("rel");
+    if(motorState == off) {  // change motor power state
+      motorState = on;
+      Serial.println("motor on");
+      
+    }
+    else {
+      motorState = off; // change motor power state
+      Serial.println("motor off");
+      cds = on;
+     }  
+     btncase = db_release;
+ 
+}
+}
