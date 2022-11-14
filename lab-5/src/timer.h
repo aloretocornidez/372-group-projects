469ebc0be9c31a88519abc7b264cef02eb095178
JasonPFreeman <117124965+JasonPFreeman@users.noreply.github.com>
2022-11-14 11:26:20 -0700

    Add files via upload

---
+++ lab-5/src/timer.h
@@ -0,0 +1,15 @@
+// Author:        
+// Net ID:         
+// Date: 11/28/2022          
+// Assignment:     Lab 5
+//----------------------------------------------------------------------//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <avr/io.h>
+
+void initTimer1();
+void delayMs(unsigned int delay);
+
+#endif
