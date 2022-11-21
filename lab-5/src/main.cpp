8a83fcb621e8a5624c6774f08cb22aaeb80d668d
JasonPFreeman <117124965+JasonPFreeman@users.noreply.github.com>
2022-11-21 11:29:19 -0700

    Update main.cpp

---
+++ lab-5/src/main.cpp
@@ -0,0 +1,21 @@
+// Author: Alan, Jason, Jeremy
+// Net ID:
+// Date: 11/28/2022
+// Assignment:     Lab 5
+//
+// Description: This file contains a programmatic overall description of the
+// program. It should never contain assignments to special function registers
+// for the exception key one-line code such as checking the state of the pin.
+//
+// Requirements:
+//----------------------------------------------------------------------//
+
+#include <Arduino.h>
+
+#include <avr/io.h>
+#include "i2c.h"
+#include "timer.h"
+#include "switch.h"
+#include "pwm.h"
+#include "spi.h"
+#include <avr/interrupt.h>
